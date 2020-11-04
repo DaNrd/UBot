f54e6c593555585e8616ef1721041cef8ca6956f
DaNrd <aaron@keydesign.com>
2020-11-04 19:10:32 +0200

    the h-bridge now works

--- Code/ubot_bot/ubot_bot.ino
+++ Code/ubot_bot/ubot_bot.ino
@@ -6,12 +6,10 @@ Code for UBot
 /*
 Pin Assignments
 */
-const byte MOTOR_L_POSITIVE_PIN = 2; // TODO: assign correct pin
-const byte MOTOR_L_NEGATIVE_PIN = 3; // TODO: assign correct pin
-const byte MOTOR_L_PWM_PIN = 4; // TODO: assign correct pin
-const byte MOTOR_R_POSITIVE_PIN = 5; // TODO: assign correct pin
-const byte MOTOR_R_NEGATIVE_PIN = 6; // TODO: assign correct pin
-const byte MOTOR_R_PWM_PIN = 7; // TODO: assign correct pin
+const byte MOTOR_L_DIRECTION_PIN = 12; // TODO: assign correct pin
+const byte MOTOR_L_PWM_PIN = 10; // TODO: assign correct pin
+const byte MOTOR_R_DIRECTION_PIN = 13; // TODO: assign correct pin
+const byte MOTOR_R_PWM_PIN = 11; // TODO: assign correct pin
 
 /*
 Constants
@@ -29,47 +27,50 @@ void setup() {
   Serial.begin(9600);
 
   // declare pin types
-  pinMode(MOTOR_L_POSITIVE_PIN,OUTPUT);
-  pinMode(MOTOR_L_NEGATIVE_PIN,OUTPUT);
+  pinMode(MOTOR_L_DIRECTION_PIN,OUTPUT);
   pinMode(MOTOR_L_PWM_PIN,OUTPUT);
-  pinMode(MOTOR_R_POSITIVE_PIN,OUTPUT);
-  pinMode(MOTOR_R_NEGATIVE_PIN,OUTPUT);
+  pinMode(MOTOR_R_DIRECTION_PIN,OUTPUT);
   pinMode(MOTOR_R_PWM_PIN,OUTPUT);
 
   // set initial motor direction to 'positive'
-  digitalWrite(MOTOR_L_POSITIVE_PIN,HIGH);
-  digitalWrite(MOTOR_L_NEGATIVE_PIN,LOW);
-  digitalWrite(MOTOR_R_POSITIVE_PIN,LOW);
-  digitalWrite(MOTOR_R_NEGATIVE_PIN,LOW);
+  digitalWrite(MOTOR_L_DIRECTION_PIN,HIGH);
+  digitalWrite(MOTOR_R_DIRECTION_PIN,LOW);
 
   // testing functions
-  motor('R',9);
-  motor('L',9);
+//  motor('R',100);
+//  motor('L',9);
 }
 
-void loop() {
+void loop() {.  
+  readInput();
+}
+
+void readInput() {
+  String received = "";
+  if(Serial.available()) {
+    received += char(Serial.read());   // read one byte from serial buffer and save to data_rcvd
+  }
+  if(received.length() != 0){
+    motor('R',100);
+  }
 }
 
 void motor(char side, float value) {
-  int pwmOutput = map(abs(value), 0 , 99, 0, 255); // map value from 0 to 255 //TODO: change 1023 to actual useful value
+  int pwmOutput = map(abs(value), 0 , 100, 0, 255); // map value from 0 to 255 //TODO: change 1023 to actual useful value
   if(side == 'R'){
     if(value>0){
-      digitalWrite(MOTOR_R_POSITIVE_PIN,HIGH);
-      digitalWrite(MOTOR_R_NEGATIVE_PIN,LOW);
+      digitalWrite(MOTOR_R_DIRECTION_PIN,HIGH);
     } else if(value<0){
-      digitalWrite(MOTOR_R_POSITIVE_PIN,LOW);
-      digitalWrite(MOTOR_R_NEGATIVE_PIN,HIGH);
+      digitalWrite(MOTOR_R_DIRECTION_PIN,LOW);
     }
     analogWrite(MOTOR_R_PWM_PIN,pwmOutput);
     Serial.print("MOTOR_R = ");
     Serial.println(pwmOutput);
   } else if(side == 'L'){
     if(value>0){
-      digitalWrite(MOTOR_L_POSITIVE_PIN,HIGH);
-      digitalWrite(MOTOR_L_NEGATIVE_PIN,LOW);
+      digitalWrite(MOTOR_L_DIRECTION_PIN,HIGH);
     } else if(value<0){
-      digitalWrite(MOTOR_L_POSITIVE_PIN,LOW);
-      digitalWrite(MOTOR_L_NEGATIVE_PIN,HIGH);
+      digitalWrite(MOTOR_R_DIRECTION_PIN,HIGH);
     }
     analogWrite(MOTOR_L_PWM_PIN,pwmOutput);
     Serial.print("MOTOR_L = ");
